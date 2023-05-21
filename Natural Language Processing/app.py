56111e278c885d46e07919b5e6cb182baa707c17
Nilesh Bhanot <newbie10003-code@github.com>
2023-05-22 00:28:55 +0530

    Changes

--- Natural Language Processing/app.py
+++ Natural Language Processing/app.py
@@ -6,9 +6,7 @@ from sentimentAnalysis import sentimentAnalysis
 
 st.header("Sentiment Analysis Of Tweets")
 
-tweets = ['agdfhfbnfvri'] * 16033
 tweet = (st.text_input(label = "Enter a tweet"))
-tweets[0] = (tweet)
 click = st.button("Submit")
 
 model = joblib.load("model.pkl")
@@ -16,12 +14,12 @@ model = joblib.load("model.pkl")
 if click:
 
     # Preprocessing
-    tweets[0] = sentimentAnalysis.clean_tags(tweets[0])
-    tweets[0] = sentimentAnalysis.clean_punctuations(tweets[0])
-    tweets[0] = sentimentAnalysis.removeHashtags(tweets[0])
+    tweet = sentimentAnalysis.clean_tags(tweet)
+    tweet = sentimentAnalysis.clean_punctuations(tweet)
+    tweet = sentimentAnalysis.removeHashtags(tweet)
 
     count_vect = CountVectorizer(stop_words = 'english')
-    count_vect = count_vect.fit_transform(tweets)
+    count_vect = count_vect.fit_transform(tweet)
     count_vect = count_vect.astype(np.float64)
 
     st.write(count_vect)
